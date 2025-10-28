cf068da4f46972ddb36919b3574adf886c94292a
Mayank Meghwal <mayankmeg207@gmail.com>
2025-10-28 18:20:31 +0530

    Best model with hyperparameter tuned

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,9 @@ import pandas as pd
 import dill # for creating a pickle file
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
+# Saving the model
 def save_object(file_path,obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -19,12 +21,47 @@ def save_object(file_path,obj):
     
     except Exception as e:
         raise CustomException(e,sys)
-    
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+
+
+# Model training
+# def evaluate_models(x_train,y_train,x_test,y_test,models):
+#     try:
+#         report = {}
+#         for i in range(len(list(models))):
+#             model = list(models.values())[i]
+            
+#             # Train model
+#             model.fit(x_train,y_train)
+            
+#             # Prediction
+#             y_train_pred = model.predict(x_train)
+#             y_test_pred = model.predict(x_test)
+            
+#             # R2 score
+#             train_model_score = r2_score(y_train,y_train_pred)
+#             test_model_score = r2_score(y_test,y_test_pred)
+            
+#             report[list(models.keys())[i]] = test_model_score
+#         return report
+#     except Exception as e:
+#         raise CustomException(e,sys)
+
+
+
+# Model Training with Hyper Parameter Tuning
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+            
+            # GridSearchCV
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+            
+            # Set best parameters
+            model.set_params(**gs.best_params_)
             
             # Train model
             model.fit(x_train,y_train)
@@ -41,3 +78,11 @@ def evaluate_models(x_train,y_train,x_test,y_test,models):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
